ab0e19d230e4aefa56e42b6d559f6f61c049bac5
muhammadderic <muhammad.donny.e@gmail.com>
2024-08-12 06:27:23 +0700

    Setup project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "img upload",
+  description: "The app for uploading images with various services",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
